14429f4dcdcb246162f72f78410421c0e0dd4b2c
lkco <lkco>
2011-05-18 08:14:09 +0000

    add gplv2 licence in php file

--- conf.php
+++ conf.php
@@ -1,18 +1,22 @@
 <?php
 /*
- * Centreon is developped with GPL Licence 2.0 :
- * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
- * Developped by : Julien Mathis - Romain Le Merlus - Christophe Coraboeuf
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  * 
- * The Software is provided to you AS IS and WITH ALL FAULTS.
- * Centreon makes no representation and gives no warranty whatsoever,
- * whether express or implied, and without limitation, with regard to the quality,
- * any particular or intended purpose of the Software found on the Centreon web site.
- * In no event will Centreon be liable for any direct, indirect, punitive, special,
- * incidental or consequential damages however they may arise and even if Centreon has
- * been previously advised of the possibility of such damages.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  * 
- * For information : contact@centreon.com
+ * GPL License: http://www.gnu.org/licenses/gpl-2.0.txt
+ * 
+ * Developped by : 
+ *   - Christophe Coraboeuf
+ *   - Charles Judith 
+ *   - Olivier LI KIANG CHEONG
+ *   - Linagora
  */
  
 // Be Carefull with internal_name, it's case sensitive (with directory module name)
